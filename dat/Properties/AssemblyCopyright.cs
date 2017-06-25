e361183f91706f84afd38fad1f59abb1fbbce41f
Jose Luis <shoot@chbshoot.me>
2017-06-25 14:05:58 -0400

    finishing touches

---
+++ dat/Properties/AssemblyCopyright.cs
@@ -0,0 +1,4 @@
+using System;
+using System.Reflection;
+
+[assembly: AssemblyCopyright("Copyright © ChbShoot 2017")]
--- src/Tools/Extensions.cs
+++ src/Tools/Extensions.cs
@@ -1,4 +1,22 @@
-﻿using System.Collections.Generic;
+﻿/*
+       This file is part of Terraria Inventory Editor
+                            Copyright © 2017 Jose Luis, Anthony Wolfe
+
+    Terraria Inventory Editor is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Terraria Inventory Editor is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Terraria Inventory Editor.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 using TerraLimb;
